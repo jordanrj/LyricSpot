d70096d90d0e3e331b2d68a806b4336df2757f0f
jordanrj <jordanrjones33@gmail.com>
2018-05-25 01:27:22 -0700

    fixed redirect url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var login = false;
 if (login === false) {
-    window.location = "https://accounts.spotify.com/authorize?client_id=1e456c18bc9d472b9a8e7a76fb2e0965&redirect_uri=https:%2F%2Fjordanrj.github.io%2FLyricSpot&scope=user-read-private%20user-read-email&response_type=token";
+    window.location = "https://accounts.spotify.com/authorize?client_id=1e456c18bc9d472b9a8e7a76fb2e0965&redirect_uri=https:%2F%2Fjordanrj.github.io%2FLyricSpot%2F&scope=user-read-private%20user-read-email&response_type=token";
 }
 
 
