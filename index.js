dc60f0e0adccd3cb4ae9bb5511bbb12ca7ecdd17
jordanrj <jordanrjones33@gmail.com>
2018-05-25 01:23:21 -0700

    initial redirect for user auth.

---
+++ index.js
@@ -0,0 +1,16 @@
+var login = false;
+if (login === false) {
+    window.location = "https://accounts.spotify.com/authorize?client_id=1e456c18bc9d472b9a8e7a76fb2e0965&redirect_uri=https:%2F%2Fjordanrj.github.io%2FLyricSpot&scope=user-read-private%20user-read-email&response_type=token";
+}
+
+
+
+/*var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(xhr.responseText);
+    }
+}
+xhr.open("GET", );
+https://cors-anywhere.herokuapp.com/
+xhr.send();*/
